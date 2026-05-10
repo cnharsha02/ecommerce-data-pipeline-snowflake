f1ad83119e771a35166846550fbf029982eb6e54
cnharsha02 <cnharsha02@gmail.com>
2026-05-10 17:08:18 +0530

    Add CSV file format SQL

---
+++ sql/02_ingestion/02_file_format.sql
@@ -0,0 +1,13 @@
+USE SCHEMA ECOMMERCE_DB.STAGING;
+
+CREATE OR REPLACE FILE FORMAT CSV_FORMAT
+    TYPE = 'CSV'
+    FIELD_DELIMITER = ','
+    SKIP_HEADER = 1
+    FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+    NULL_IF = ('NULL', 'null', '')
+    EMPTY_FIELD_AS_NULL = TRUE
+    COMMENT = 'Standard CSV file format for e-commerce data';
+
+-- Verify the file format was created
+SHOW FILE FORMATS IN SCHEMA ECOMMERCE_DB.STAGING;
